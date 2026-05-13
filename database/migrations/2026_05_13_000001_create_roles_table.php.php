dbc6a208182c19784494765576df9071711c381e
pirdaw <akunhiddenpirdaw04@gmail.com>
2026-05-13 10:54:53 +0700

    mgrate

--- database/migrations/2026_05_13_000001_create_roles_table.php.php
+++ database/migrations/2026_05_13_000001_create_roles_table.php.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pasiens', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        Schema::create('roles', function (Blueprint $table) {
+    $table->id();
+    $table->string('nama_role');
+    $table->timestamps();
+});
     }
 
     /**
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pasiens');
+        Schema::dropIfExists('roles');
     }
 };
---
+++ database/migrations/2026_05_13_131954_create_pasiens_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('pasiens', function (Blueprint $table) {
+        $table->id();
+        $table->string('nama');
+        $table->string('nik', 16)->unique();
+        $table->integer('umur');
+        $table->enum('jenis_kelamin', ['L', 'P']);
+        $table->enum('golongan_darah', [
+            'A',
+            'B',
+            'AB',
+            'O'
+            ])->nullable();
+        $table->string('golongandarah');
+        $table->string('agama');
+        $table->text('alamat');
+        $table->string('pekerjaan');
+        $table->string('nohp');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pasiens');
+    }
+};
