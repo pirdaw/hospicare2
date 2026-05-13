872461c6b6a43365b71aa029925b9332b05e77d3
pirdaw <akunhiddenpirdaw04@gmail.com>
2026-05-13 11:16:59 +0700

    fatal error

--- database/migrations/2026_05_13_153218_create_kunjungans_table.php
+++ database/migrations/2026_05_13_153218_create_kunjungans_table.php
@@ -10,19 +10,46 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-{
-    Schema::create('kunjungans', function (Blueprint $table) {
-        $table->id();
-        $table->date('tanggal_kunjungan');
-        $table->string('poli_tujuan');
-        $table->text('keluhan');
-        $table->string('jenis_pembayaran');
-        $table->string('rujukan_dari');
-        $table->string('status');
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('kunjungans', function (Blueprint $table) {
+            $table->id();
 
+            // relasi pasien
+            $table->foreignId('pasien_id')
+                  ->constrained('pasiens')
+                  ->onDelete('cascade');
+
+            // relasi poli
+            $table->foreignId('poli_id')
+                  ->constrained('polis')
+                  ->onDelete('cascade');
+
+            // petugas input
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->onDelete('cascade');
+
+            $table->date('tanggal_kunjungan');
+
+            $table->text('keluhan');
+
+            $table->enum('jenis_pembayaran', [
+                'umum',
+                'bpjs',
+                'swasta'
+            ]);
+
+            $table->string('rujukan_dari')->nullable();
+
+            $table->enum('status', [
+                'menunggu',
+                'diperiksa',
+                'selesai'
+            ]);
+
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
