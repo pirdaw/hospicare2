0979dceea2b6fad2e9e1fbbb6d94fd84ffa6eaa7
Lenn-tech <vlemingo1401@gmail.com>
2026-05-14 14:26:30 +0700

    Pembaruan-Valent

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,66 +1,67 @@
-<nav class="navbar navbar-expand navbar-light topbar mb-4 static-top shadow">
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-    <!-- Sidebar Toggle -->
+    <!-- Sidebar Toggle (Mobile) -->
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
     </button>
 
-    <!-- Search -->
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-        <div class="input-group">
-
-            <input type="text"
-                class="form-control bg-light border-0 small"
-                placeholder="Cari Data..."
-            >
-
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search fa-sm"></i>
-                </button>
-            </div>
-
-        </div>
-    </form>
-
-    <!-- Right Navbar -->
     <ul class="navbar-nav ml-auto">
 
-        <!-- User -->
+        <!-- Info Role -->
+        <li class="nav-item d-none d-sm-flex align-items-center mr-3">
+            <span class="badge badge-secondary" style="font-size:12px; padding: 6px 12px; border-radius:20px;">
+                {{ ucfirst(str_replace('_', ' ', Auth::user()->role->nama_role ?? '-')) }}
+            </span>
+        </li>
+
+        <div class="topbar-divider d-none d-sm-block"></div>
+
+        <!-- User Info + Logout -->
         <li class="nav-item dropdown no-arrow">
-
-            <a class="nav-link dropdown-toggle"
-                href="#"
-                id="userDropdown"
-                role="button"
-                data-toggle="dropdown">
-
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                    Admin
+                    {{ Auth::user()->nama }}
                 </span>
-
-                <img class="img-profile rounded-circle"
-                    src="{{ asset('assets/img/undraw_profile.svg') }}">
+                <i class="fas fa-user-circle fa-fw fa-lg text-gray-400"></i>
             </a>
 
-            <!-- Dropdown -->
-            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in">
-
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
-                </a>
-
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <div class="dropdown-header">
+                    <strong>{{ Auth::user()->nama }}</strong><br>
+                    <small class="text-muted">{{ Auth::user()->email }}</small>
+                </div>
                 <div class="dropdown-divider"></div>
-
-                <a class="dropdown-item" href="{{ url('/') }}">
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                 </a>
-
             </div>
         </li>
 
     </ul>
-
 </nav>
+
+<!-- Logout Modal -->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Konfirmasi Logout</h5>
+                <button class="close" type="button" data-dismiss="modal">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Apakah kamu yakin ingin keluar dari sesi ini?
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+                <form action="{{ route('logout') }}" method="POST" style="display:inline;">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Logout</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
